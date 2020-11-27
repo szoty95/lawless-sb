1b745a5fd8ef0e3a35eb9d52019913d068785437
Bodor Máté <bmate711@gmial.com>
2020-11-27 17:39:59 +0100

    Create static header

---
+++ lawless/lawless-frontend/src/components/Header.tsx
@@ -0,0 +1,48 @@
+import { AppBar, Box, Button, Toolbar, Typography } from "@material-ui/core";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router";
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: "black",
+  },
+
+  logo: {
+    fontFamily: "'Salsa', cursive",
+  },
+
+  button: {
+    padding: '8px 48px',
+    borderRadius: 32,
+    marginLeft: 32,
+  },
+
+});
+
+const Header: React.FC = () => {
+  const styles = useStyles();
+  const history = useHistory();
+
+  return (
+    <AppBar className={styles.root} position="static">
+      <Toolbar>
+        <Box display="flex" justifyContent="space-between" flex={1} alignItems="center">
+          <Typography
+            onClick={() => history.push("/caffs")}
+            className={styles.logo}
+            variant="h3"
+          >
+            Outlaws
+          </Typography>
+          <Box display="flex">
+            <Button className={styles.button} variant="contained" color="primary">Upload</Button>
+            <Button className={styles.button} variant="contained"> Login</Button>
+          </Box>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
