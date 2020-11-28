aac9127c05cee57c5fb042abc01c6444268672cd
Szalontai Jenő <jeno96@gmail.com>
2020-11-28 12:05:06 +0100

    addedoutput name

---
+++ cmake-build-debug/CMakeFiles/caffparser.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/caffparser.dir/main.cpp.obj"
+  "caffparser.exe"
+  "caffparser.exe.manifest"
+  "caffparser.pdb"
+  "libcaffparser.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/caffparser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
