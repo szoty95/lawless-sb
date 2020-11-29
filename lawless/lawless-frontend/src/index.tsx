b967549ddf185a7e49c4098be36642f7b6e0221b
Chif Gergő <chif.gergo@cloud.bme.hu>
2020-11-29 21:58:21 +0100

    Add sentry integration

--- lawless/lawless-frontend/src/index.tsx
+++ lawless/lawless-frontend/src/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: 'https://d00989f44269478ebf1ca1714d6382b5@o484192.ingest.sentry.io/5537075',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
