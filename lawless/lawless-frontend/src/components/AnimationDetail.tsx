e73ac5292a3e8a405172c703ea973d73e66d45ae
Chif Gergő <chif.gergo@cloud.bme.hu>
2020-11-29 16:04:46 +0100

    Remov econsole log

--- lawless/lawless-frontend/src/components/AnimationDetail.tsx
+++ lawless/lawless-frontend/src/components/AnimationDetail.tsx
@@ -1,50 +1,68 @@
-import { Button, Chip, Grid, Typography } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import Skeleton from '@material-ui/lab/Skeleton';
 import React from 'react';
+import { Redirect } from 'react-router';
+import { useAuthToken } from '../hooks/useAuthToken';
+import useDeleteCaff from '../hooks/useDelete';
+import useUserContext from '../hooks/useUserContext';
+import { IDetailsCaffResp } from '../swagger';
+import DeleteDialog from './DeleteDialog';
+import EditCaffDialog from './EditCaffDetailForm';
 
-const AnimationDetail: React.FC = () => {
+const ADMIN = 'ROLE_ADMIN';
+
+type AnimationDetailProps = {
+  animation: IDetailsCaffResp;
+};
+
+const AnimationDetail: React.FC<AnimationDetailProps> = ({ animation }) => {
+  const { user } = useUserContext();
+  const { authToken } = useAuthToken();
+  const [result, deleteCaff] = useDeleteCaff();
+
+  const handleDelete = () => {
+    deleteCaff({
+      data: animation.id,
+      authToken: authToken as string,
+    });
+  };
+
+  if (result.data) {
+    return <Redirect to="/" />;
+  }
+
   return (
     <Grid container direction="column">
-      <Typography variant="h5">Title</Typography>
+      <Typography variant="h5">{animation.name}</Typography>
       <Grid container>
         <Grid item xs={12} sm={6} container direction="column">
           <Skeleton variant="rect" width={500} height={400} />
         </Grid>
         <Grid item xs={12} sm={6} container direction="column" spacing={2}>
           <Grid item container justify="space-between" alignItems="center">
-            <Grid item>
-              <Typography variant="h6">Kiss Pista</Typography>
-              <Typography variant="subtitle1">2020. 02. 21</Typography>
+            <Grid item xs={6}>
+              <Typography variant="h6">
+                {animation.userPersonalData?.firstName} {animation.userPersonalData?.lastName}
+              </Typography>
+              <Typography variant="subtitle1">{animation.uploaded}</Typography>
             </Grid>
-            <Grid item>
-              <Button variant="contained" color="primary">
-                Edit
-              </Button>
-            </Grid>
+            {user && (user.roles.includes(ADMIN) || user.userId === animation.userId) && (
+              <Grid xs={6} item container spacing={2}>
+                <Grid item>
+                  <EditCaffDialog animation={animation} />
+                </Grid>
+                <Grid item>
+                  <DeleteDialog onDelete={handleDelete} />
+                </Grid>
+              </Grid>
+            )}
           </Grid>
 
           <Grid item>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis nam fusce urna, nec. Egestas orci molestie
-              felis, amet, at enim, odio montes, ornare. Hendrerit et a sagittis id a sit nunc, congue massa. Aliquam,
-              in faucibus pulvinar convallis sed tristique. Faucibus augue in duis lacus, ornare. Ipsum ac at odio
-              aliquam id natoque fermentum lobortis. Massa hac tristique consectetur enim praesent egestas. Amet, nunc
-              risus hac facilisis nulla vulputate. Elementum ut rhoncus scelerisque eu turpis commodo hendrerit
-              interdum. Pellentesque vestibulum lectus arcu ornare ultricies. Feugiat elit feugiat nunc ut id pulvinar
-              quis. Elementum lectus ornare purus sollicitudin in.
-            </Typography>
+            <Typography>{animation.description}</Typography>
           </Grid>
-
-          <Grid item container spacing={2}>
-            <Grid item>
-              <Chip label="asdasdasd" />
-            </Grid>
-            <Grid item>
-              <Chip label="asdasdasd" />
-            </Grid>
-            <Grid item>
-              <Chip label="asdasdasd" />
-            </Grid>
+          <Grid item>
+            <Typography variant="h6">Price: {animation.price}</Typography>
           </Grid>
         </Grid>
       </Grid>
