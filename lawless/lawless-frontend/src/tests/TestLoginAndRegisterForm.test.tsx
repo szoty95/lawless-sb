2c95f2df7e8046461ebac0bd86177b0db0e62b73
Chif Gergő <chif.gergo@cloud.bme.hu>
2020-11-29 21:50:09 +0100

    Add more test cases

--- lawless/lawless-frontend/src/tests/TestLoginAndRegisterForm.test.tsx
+++ lawless/lawless-frontend/src/tests/TestLoginAndRegisterForm.test.tsx
@@ -1,9 +1,10 @@
 import { TextField } from '@material-ui/core';
-import { shallow, configure } from 'enzyme';
+import { shallow, configure, mount } from 'enzyme';
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import LoginForm from '../components/LoginForm';
 import CryptoJS from 'crypto-js';
+import RegisterDialog from '../components/RegisterDialog';
 
 configure({ adapter: new Adapter() });
 
@@ -39,33 +40,71 @@ describe('LoginForm test', () => {
 
     expect(wrapper.find('#password').props().value).toEqual('testpassword');
   });
+});
 
-  it('Submit login form', async () => {
-    const wrapper = shallow(<LoginForm />);
+describe('RegisterForm test', () => {
+  const register = <RegisterDialog open setOpen={() => {}} />;
 
-    const mockedLogin = jest.fn();
+  it('Test RegisterForm rendering', () => {
+    const wrapper = shallow(register);
+    expect(wrapper).toBeTruthy();
+  });
 
-    jest.mock('../hooks/useLogin', () => {
-      return jest.fn(() => {});
-    });
+  it('Test user input form working', () => {
+    const wrapper = shallow(register);
+    expect(wrapper.find(TextField)).toHaveLength(6);
+  });
 
+  it('Username input change', () => {
+    const wrapper = shallow(register);
     wrapper
       .find('#userName')
       .simulate('change', { persist: () => {}, target: { name: 'userName', value: 'testuser' } });
 
+    expect(wrapper.find('#userName').props().value).toEqual('testuser');
+  });
+
+  it('Password1 change', () => {
+    const wrapper = shallow(register);
+
     wrapper
-      .find('#password')
-      .simulate('change', { persist: () => {}, target: { name: 'password', value: 'testpassword' } });
+      .find('#password1')
+      .simulate('change', { persist: () => {}, target: { name: 'password1', value: 'testpassword' } });
 
-    wrapper.find('form').simulate('submit', {
-      preventDefault: () => {}, // no op
-    });
+    expect(wrapper.find('#password1').props().value).toEqual('testpassword');
+  });
 
-    const password = CryptoJS.SHA256('testpassword').toString();
+  it('Password2 change', () => {
+    const wrapper = shallow(register);
 
-    expect(mockedLogin).toBeCalled();
-    expect(mockedLogin).toBeCalledWith({
-      data: { username: 'testuser', password, email: '' },
-    });
+    wrapper
+      .find('#password2')
+      .simulate('change', { persist: () => {}, target: { name: 'password2', value: 'testpassword' } });
+
+    expect(wrapper.find('#password2').props().value).toEqual('testpassword');
+  });
+
+  it('Last Name change', () => {
+    const wrapper = shallow(register);
+
+    wrapper.find('#lastName').simulate('change', { persist: () => {}, target: { name: 'lastName', value: 'test' } });
+
+    expect(wrapper.find('#lastName').props().value).toEqual('test');
+  });
+
+  it('First Name change', () => {
+    const wrapper = shallow(register);
+
+    wrapper.find('#firstName').simulate('change', { persist: () => {}, target: { name: 'firstName', value: 'test' } });
+
+    expect(wrapper.find('#firstName').props().value).toEqual('test');
+  });
+
+  it('Email change', () => {
+    const wrapper = shallow(register);
+
+    wrapper.find('#email').simulate('change', { persist: () => {}, target: { name: 'email', value: 'test@test.hu' } });
+
+    expect(wrapper.find('#email').props().value).toEqual('test@test.hu');
   });
 });
